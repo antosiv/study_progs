e4096fc7aa337342f349e86846c1164ce1caabb4
antosiv <anton6722@gmail.com>
2018-08-28 21:09:21 +0300

    working 2-level depth ai vs ai version

--- python/prak_2sem/main.py
+++ python/prak_2sem/main.py
@@ -2,19 +2,21 @@ import pygame
 from pygame.locals import *
 import numpy as np
 import subprocess
-
+import random
+from time import sleep
 
 class GameError(Exception):
-    pass
+    def __init__(self, msg):
+        super().__init__(msg)
 
 
 class Checkers:
-    def __init__(self, width=640, height=480, speed=10, g_type='ai_vs_ai', max_depth=2):
+    def __init__(self, width=640, height=480, speed=1, g_type='ai_vs_ai', recursion_depth=2):
         self.width = width
         self.height = height
         self.type = g_type
         self.screen_size = width, height
-        self.max_depth = max_depth
+        self.recursion_depth = recursion_depth
 
         self.screen = pygame.display.set_mode(self.screen_size)
 
@@ -29,26 +31,7 @@ class Checkers:
         if self.type == 'ai_vs_ai':
             self.run_ava()
         else:
-            raise GameError()
-
-    def run_ava(self):
-        pygame.init()
-        clock = pygame.time.Clock()
-        pygame.display.set_caption('Checkers')
-
-        self._draw_board()
-        self.init_field()
-        self._draw_figures()
-        pygame.display.flip()
-
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    running = False
-            # pygame.display.flip()
-            clock.tick(self.speed)
-        pygame.quit()
+            raise GameError('Unknown game type')
 
     def _draw_board(self):
         colors = [pygame.Color('black'), pygame.Color('white')]
@@ -60,7 +43,7 @@ class Checkers:
 
     def _draw_figures(self):
         if self.figures_map is None:
-            raise GameError()
+            raise GameError('Game field is not inited')
         for i in range(8):
             for j in range(8):
                 f_color = None
@@ -73,14 +56,15 @@ class Checkers:
                 elif self.figures_map[i, j] == -2:
                     f_color = pygame.Color('white')
                 if f_color is not None:
-                    pygame.draw.circle(self.screen,
-                                       f_color,
-                                       (
-                                           j * self.cell_width + self.cell_width // 2,
-                                           i * self.cell_height + self.cell_height // 2
-                                        ),
-                                       min(self.cell_width, self.cell_height) // 2
-                                       )
+                    pygame.draw.circle(
+                        self.screen,
+                        f_color,
+                        (
+                            j * self.cell_width + self.cell_width // 2,
+                            i * self.cell_height + self.cell_height // 2
+                        ),
+                        min(self.cell_width, self.cell_height) // 2
+                    )
 
     def init_field(self):
         self.figures_map = np.zeros((8, 8), dtype=np.int32)
@@ -93,18 +77,140 @@ class Checkers:
         self.figures_map[6, mask_1] = -1
         self.figures_map[7, mask_2] = -1
 
-    def get_opt_step(self, uid):
-        params = ['./s_calc.out']
-        for i in range(8):
-            for j in range(8):
-                params.append(str(self.figures_map[i, j]))
+    @staticmethod
+    def get_neighbors(pos):
+        return [
+            ((pos[0] - 1, pos[1] - 1), (pos[0] - 2, pos[1] - 2)),
+            ((pos[0] + 1, pos[1] - 1), (pos[0] + 2, pos[1] - 2)),
+            ((pos[0] - 1, pos[1] + 1), (pos[0] - 2, pos[1] + 2)),
+            ((pos[0] + 1, pos[1] + 1), (pos[0] + 2, pos[1] + 2))
+        ]
 
-        params.append(str(uid))
-        params.append(str(self.max_depth))
+    @staticmethod
+    def in_shape(pos, shape):
+        return 0 <= pos[0] < shape[0] and 0 <= pos[1] < shape[1]
 
-        return map(int, subprocess.run(params, stdout=subprocess.PIPE).stdout.split())
+    def get_eat_chains(self, pos, fig_type, fig_map, root_pos=None):
+        res = {}
+        for neighbor in self.get_neighbors(pos):
+            if self.in_shape(neighbor[0], fig_map.shape) and\
+                    self.in_shape(neighbor[1], fig_map.shape) and \
+                    fig_map[neighbor[0]] == -1 * fig_type and \
+                    fig_map[neighbor[1]] == 0 and\
+                    (root_pos is None or root_pos[0] != neighbor[1][0] or root_pos[1] != neighbor[1][1]):
+                    res[neighbor[1]] = self.get_eat_chains(neighbor[1], fig_type, fig_map, root_pos=pos)
+        return res
+
+    def get_chains_list(self, root_list, all_list, root_dict):
+        if len(root_dict) == 0:
+            return
+        else:
+            keys = list(root_dict.keys())
+            for key in keys[1:]:
+                all_list.append(root_list.copy())
+                all_list[-1].append(key)
+                self.get_chains_list(all_list[-1], all_list, root_dict[key])
+
+            root_list.append(keys[0])
+            self.get_chains_list(root_list, all_list, root_dict[keys[0]])
+
+    def get_possible_steps(self, fig_type, fig_map):
+        steps = []
+        eats = []
+        for figure in np.argwhere(fig_map == fig_type):
+
+            # steps
+            for pos in (figure[0] + fig_type, figure[1] + 1), (figure[0] + fig_type, figure[1] - 1):
+                if self.in_shape(pos, fig_map.shape):
+                    if fig_map[pos] == 0:
+                        steps.append((tuple(figure), pos))
+            # eats
+            chains = self.get_eat_chains(figure, fig_type, fig_map)
+            if len(chains) > 0:
+                eats.append([])
+                self.get_chains_list(eats[-1], eats, {tuple(figure): chains})
+
+        return steps, eats
+
+    def make_step(self, source_map, step):
+        res_map = source_map.copy()
+        if len(step) == 2 and max([abs(step[0][0] - step[1][0]), abs(step[0][1] - step[1][1])]) == 1:
+            res_map[step[1]] = res_map[step[0]]
+            res_map[step[0]] = 0
+        else:
+            for i in range(1, len(step)):
+                res_map[step[i - 1][0] + (step[i][0] - step[i - 1][0]) // 2, step[i - 1][1] + (step[i][1] - step[i - 1][1]) // 2] = 0
+                res_map[step[i]] = res_map[step[i - 1]]
+                res_map[step[i - 1]] = 0
+        return res_map
+
+    def get_step_tree(self, root_dict, fig_type, recursion_depth, fig_map):
+        if recursion_depth == 0:
+            return
+        steps = self.get_possible_steps(fig_type, fig_map)
+        for step in [*steps[0], *steps[1]]:
+            t_step = tuple(step)
+            root_dict[t_step] = dict()
+            root_dict[t_step]['map'] = self.make_step(fig_map, step)
+            root_dict[t_step]['score'] = root_dict[t_step]['map'].sum()
+            root_dict[t_step]['sons'] = dict()
+            self.get_step_tree(root_dict[t_step]['sons'], -1 * fig_type, recursion_depth - 1, root_dict[t_step]['map'])
+
+    def get_optimal_step(self, fig_type):
+        tree = dict()
+        self.get_step_tree(tree, fig_type, self.recursion_depth, self.figures_map)
+        scores = dict()
+        best_score = None
+        for step, elem in tree.items():
+            step_list = [val['score'] for val in elem['sons'].values()]
+            if len(step_list) > 0:
+                if fig_type == 1:
+                    scores[step] = min(step_list)
+                else:
+                    scores[step] = max(step_list)
+            else:
+                continue
+            if best_score is None or scores[step] > best_score and fig_type == 1 or scores[step] < best_score and fig_type == -1:
+                best_score = scores[step]
+
+        steps = list(filter(lambda x: True if scores[x] == best_score else False, scores.keys()))
+        if len(steps) == 0:
+            return
+        else:
+            ind = random.randint(0, len(steps) - 1)
+            return steps[ind], tree[steps[ind]]['map']
+
+    def run_ava(self):
+        pygame.init()
+        pygame.display.set_caption('Checkers')
+
+        self._draw_board()
+        self.init_field()
+        self._draw_figures()
+        pygame.display.flip()
+
+        running = True
+        figure_type = 1
+        while running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+            tmp = self.get_optimal_step(figure_type)
+            if tmp is None:
+                continue
+            else:
+                self.figures_map = tmp[1]
+                self._draw_board()
+                self._draw_figures()
+                pygame.display.flip()
+                figure_type *= -1
+                sleep(self.speed)
+        pygame.quit()
+
+    def test(self):
+        self.run_ava()
 
 
 if __name__ == '__main__':
     a = Checkers()
-    a.run_g()
+    a.test()
